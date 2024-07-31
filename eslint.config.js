c6436eab9f2783925efe9c2f2c29bc004cf92432
wen <wen@trip.com>
2024-07-31 10:39:03 +0800

    fix eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -2,8 +2,17 @@
 import antfu from '@antfu/eslint-config';
 
 export default antfu({
-  vue: true,
-  typescript: true,
+  vue: {
+    overrides: {
+      'vue/operator-linebreak': ['error', 'before'],
+      'vue/html-self-closing': 'off',
+    },
+  },
+  typescript: {
+    overrides: {
+      'ts/consistent-type-definitions': ['error', 'interface'],
+    },
+  },
 }, {
   // Remember to specify the file glob here, otherwise it might cause the vue plugin to handle non-vue files
   files: ['**/*.vue', '**/*.ts'],
